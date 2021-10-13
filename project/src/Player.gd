741499cb6fa1e9dbc117f5fd7aefe4fb15f751fa
Nathaniel <ncbarber11@gmail.com>
2021-10-13 19:05:45 -0400

    Add basic script for Player

---
+++ project/src/Player.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+var velocity := Vector2()
+var gravity := 600
+var jumpPower := -350
+var speed := 300
+var jumpped := false
+
+
+func _physics_process(delta):
+	velocity.x = 0
+	# Checking for when certian Controls are pressed
+	if Input.is_action_pressed("right"):
+		velocity.x += speed
+
+	if Input.is_action_pressed("left"):
+		velocity.x -= speed
+
+	if Input.is_action_pressed("jump"):
+		if is_on_floor():
+			velocity.y = jumpPower
+			jumpped = true
+
+	velocity.y += gravity * delta
+	
+	if jumpped and is_on_floor():
+		jumpped = false
